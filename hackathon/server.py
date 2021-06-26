10a0a24cc28ac50d7e79b8f1821b8fff4ec1f2f0
Andrea <andrea.mendez.ortiz@gmail.com>
2021-06-26 11:07:51 -0400

    clases creadas

--- hackathon/server.py
+++ hackathon/server.py
@@ -6,6 +6,12 @@ class Carta:
     def __init__(self, nombre, costo):
         self.nombre = nombre
         self.costo = costo
+    
+    def display_info(self):
+        return(f"{self.nombre}, {self.poder} y {self.resistencia}")
+        
+    def __repr__(self):
+        return self
         
 
 class Ninja(Carta):
@@ -13,56 +19,58 @@ class Ninja(Carta):
         super().__init__(nombre, costo)
         self.poder = poder
         self.resistencia = resistencia
-    
+
+
     #rival = carta ninja del oponente
     def attack(self, oponente):
         if isinstance(oponente, Ninja) == False:
             print('Esta carta no es del tipo Ninja')
             return
-        # ahora puedo atacar al oponente
-            oponente.gemas -= self.poder
-            print (f"Has conseguido quitarle {self.poder} gemas a {oponente.nombre}")
-        else:    
-            oponente.rival.resistencia -= self.poder
-            print (f"Has conseguido dañar en  {self.poder} puntos a {oponente.rival} de {oponente.nombre}")
+        oponente.resistencia -= self.poder
+        print (f"Has conseguido dañar en {self.poder} puntos al {oponente.nombre}. Su nueva resistencia es {oponente.resistencia}")
 
-class Efectos():
+class Efectos(Carta):
     
-    def __init__(self, efecto, costo, texto, stat, magnitud):
-        self.costo = costo
-        self.efecto = efecto
+    def __init__(self, nombre, costo, texto, stat, magnitud):
+        super().__init__(nombre, costo)
         self.texto = texto
         self.stat = stat
         self.magnitud = magnitud
-
-    def algoritmoDuro(self):
-        self.costo 
-        self.resistencia += self.magnitud
     
-    def promesaNoManejada(self, rival):
-        rival.resistencia -= 2
-    
-    def programacionEnPareja(self):
-        self.poder += 2
+    def affect(self, target):
+        if isinstance(target,Ninja) == False:
+            print('Esta carta no es del tipo Ninja')
+            return
+        
+        if self.stat == 'poder':
+            target.poder += self.magnitud
+        elif self.stat == 'resistencia':
+            target.resistencia += self.magnitud
 
+'''
 class Jugador(Ninja, Efectos):
     def __init__(self, nombre, gemas):
         self.nombre = nombre
         self.gemas = gemas
+'''
 
 #cartas ninja
 ninjarojo = Ninja('ninja_rojo', 3, 3, 4)
 ninjanegro = Ninja('ninja_negro', 4, 5, 4)
 
-#cartas efecto
-algoritmoduro = Efectos('algoritmoduro', 2, 'Aumentar la resistencia del objetivo en 3','Resistencia', 3 )
 
+#cartas efecto
+algoritmoduro = Efectos('Algoritmo Duro', 2, 'Aumentar la resistencia del objetivo en 3','resistencia', 3 )
+promesaNoManejada = Efectos("Promesa no manejada", 1, "Reducir la resistencia del objetivo en 2", "resistencia", -2)
+programacionEnPareja = Efectos("Programación en pareja", 3, "Aumentar el poder del objetivo en 2", "ataque", 2)
+
+'''
 #jugadores
 jugador1 = Jugador('Andrea', 100)
 jugador2 = Jugador('Juan', 100)
 jugador3 = Jugador('Ruben',100)
+'''
 
-jugador1.ninjarojo.algoritmoduro()
 
 import pdb; pdb.set_trace()
 
