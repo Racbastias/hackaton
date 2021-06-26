96e545a6d074d5fb95e9d2855495b6ffcd628e60
Racbastias <rac.bastias@gmail.com>
2021-06-25 20:03:52 -0400

    commit

--- hackathon/server.py
+++ hackathon/server.py
@@ -1,6 +1,41 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+class Ninja(self, nombre):
+    
+    def __init__(self, nombre, costo, poder, resistencia):
+        self.nombre = nombre
+        self.costo = costo
+        self.poder = poder
+        self.resistencia = resistencia
+    
+    def attack(self):
+        raise NotImplementedError
+
+class Efectos(Ninja):
+    
+    def __init__(self, nombre, efecto, costo, texto, stat, magnitud):
+        super().__init__(nombre)
+        self.costo = costo
+        self.efecto = efecto
+        self.texto = texto
+        self.stat = stat
+        self.magnitud = magnitud
+
+    def algoritmoduro(self):
+        self.resistencia += 3
+    
+    def promesanomanejada(self, rival):
+        rival.resistencia -= 2
+
+
+
+
+
+
+
+
 '''
 # turno 1
 red_jinja = Unit('Red Ninja', 3, 3, 7)
